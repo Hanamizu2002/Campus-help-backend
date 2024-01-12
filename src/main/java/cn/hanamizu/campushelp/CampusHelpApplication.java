c3e6bd87448786ca210e3bdf6c339f1f6d2c9113
Kyle Ma <m2069134445@icloud.com>
2024-01-12 16:15:37 +0800

    init

---
+++ src/main/java/cn/hanamizu/campushelp/CampusHelpApplication.java
@@ -0,0 +1,13 @@
+package cn.hanamizu.campushelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusHelpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusHelpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/hanamizu/campushelp/CampusHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hanamizu.campushelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusHelpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
