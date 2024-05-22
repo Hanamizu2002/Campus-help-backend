6078789a33b43e831fc4064dee04920f07daa0c0
Kyle Ma <m2069134445@icloud.com>
2024-05-22 15:03:39 +0800

    添加cos配置

--- src/main/java/cn/hanamizu/campushelp/controller/COSUtil.java
+++ src/main/java/cn/hanamizu/campushelp/controller/COSUtil.java
@@ -1,5 +1,6 @@
 package cn.hanamizu.campushelp.controller;
 
+import cn.hanamizu.campushelp.common.config.COSConfig;
 import com.qcloud.cos.COSClient;
 import com.qcloud.cos.ClientConfig;
 import com.qcloud.cos.auth.BasicCOSCredentials;
@@ -7,16 +8,26 @@ import com.qcloud.cos.auth.COSCredentials;
 import com.qcloud.cos.model.PutObjectRequest;
 import com.qcloud.cos.model.PutObjectResult;
 import com.qcloud.cos.region.Region;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 
+@Component
 public class COSUtil {
 
-    public static PutObjectResult uploadFile(MultipartFile multipartFile, String bucketName, String key) throws IOException {
-        COSCredentials cred = new BasicCOSCredentials("", "");
-        ClientConfig clientConfig = new ClientConfig(new Region("ap-shanghai"));
+    private final COSConfig cosConfig;
+
+    @Autowired
+    public COSUtil(COSConfig cosConfig) {
+        this.cosConfig = cosConfig;
+    }
+
+    public PutObjectResult uploadFile(MultipartFile multipartFile, String bucketName, String key) throws IOException {
+        COSCredentials cred = new BasicCOSCredentials(cosConfig.getSecretId(), cosConfig.getSecretKey());
+        ClientConfig clientConfig = new ClientConfig(new Region(cosConfig.getRegion()));
         COSClient cosClient = new COSClient(cred, clientConfig);
 
         // 转存文件到本地（也可以选择不转存，直接使用multipartFile的InputStream上传）
@@ -32,4 +43,3 @@ public class COSUtil {
         return putObjectResult;
     }
 }
-
